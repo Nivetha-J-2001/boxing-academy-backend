7b74cef7aad7a6fabb658cba531dbfafd9a60893
Nivetha-J-2001 <nivethajayaraj2001@gmail.com>
2022-04-21 22:59:19 +0530

    changed the package name

--- src/main/java/com/springboot/demo/repo/BatchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.demo.model.Batch;
-
-public interface BatchRepository extends JpaRepository<Batch,Long> {
-
-}
--- src/main/java/com/springboot/demo/repo/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.demo.repo;
-
-import com.springboot.demo.model.Role;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long>{
-    
-}
